4fdc51e636a4917be3012f7969dfaad57e2a9138
Kooku <koomg9599@gmail.com>
2020-02-08 02:35:27 +0900

    feat: server template 생성

---
+++ server/src/app.ts
@@ -0,0 +1,38 @@
+import * as express from 'express'
+import * as http from 'http'
+import logger from './logger'
+
+// Shutdown codes
+const errShutdown = async (server: http.Server, err?: Error) => {
+  logger.error(`Stopping server with error: ${err}`)
+  await server.close()
+  process.exit(1)
+}
+
+const graceShutdown = (signal: string) => {
+  logger.info(`Stopping server with signal: ${signal}`)
+  process.exit(0)
+}
+
+async function runServer() {
+  const app = express()
+  const server = app.listen(5000, () => {
+    logger.info('Server app listening on port 5000!')
+  })
+  try {
+  } catch (e) {
+    errShutdown(server, e)
+    throw e
+  }
+}
+runServer()
+  .then(() => {
+    logger.info('run succesfully')
+  })
+  .catch((ex: Error) => {
+    logger.error('Unable run:', ex)
+  })
+
+// Graceful shutdown - SIGINT handling
+process.on('SIGINT', () => graceShutdown('SIGINT'))
+process.on('SIGTERM', () => graceShutdown('SIGTERM'))
